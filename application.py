7262c2d53f59877e45bd7150aac80ec6c3fa68cf
Tingting Li <tl2617@columbia.edu>
2015-05-01 14:17:12 -0400

    WORKING

--- application.py
+++ application.py
@@ -23,69 +23,53 @@ application = Flask(__name__)
 # SNS HTTP request endpoint: subscribe, unsubscribe, notification
 @application.route('/minions', methods = ['POST','GET'])
 def sns():
-	headers = request.headers
-	print "headers", headers
-	print "request", request
-	arn = headers.get('X-Amz-Sns-Topic-Arn')
-	print "before request.data"
-	obj = json.loads(request.data)
-	if arn:
-		snsType = headers.get('X-Amz-Sns-Message-Type')
-		if snsType == 'SubscriptionConfirmation':
-			subscribe_url = obj[u'SubscribeURL']
+    headers = request.headers
+    print "headers", headers
+    print "request", request
+    arn = headers.get('X-Amz-Sns-Topic-Arn')
+    print "before request.data"
+    obj = json.loads(request.data)
+    if arn:
+        snsType = headers.get('X-Amz-Sns-Message-Type')
+        if snsType == 'SubscriptionConfirmation':
+            subscribe_url = obj[u'SubscribeURL']
+            f = urllib.urlopen(subscribe_url)
+            myfile = f.read()
+            print myfile
+            return '', 200
+        elif snsType == 'Notification':
+            notification_id = obj[u'MessageId']
+            message = obj[u'Message']
+            print message
+            conn = boto.sqs.connect_to_region("us-east-1")
+            q = conn.get_queue('kitkat_SQS')
+            print "made connection"
+            # global resultsFromSQS
+            tempSQS = []
+            for i in range(3):
+                rs = q.get_messages(message_attributes=['text','sentimentStat','geoLat','geoLong'])
+                text = rs[0].message_attributes['text']['string_value']
+                geoLat = rs[0].message_attributes['geoLat']['string_value']
+                geoLong = rs[0].message_attributes['geoLong']['string_value']
+                sentimentStat = rs[0].message_attributes['sentimentStat']['string_value']
+                print "SQS text = ", text
+                tempSQS.append({
+                    'text': text,
+                    'geoLat': geoLat,
+                    'geoLong': geoLong,
+                    'sentimentStat': sentimentStat
+                    })
 
-			f = urllib.urlopen(subscribe_url)
-			myfile = f.read()
-			print myfile
-
-			return '', 200
-
-		elif snsType == 'Notification':
-			notification_id = obj[u'MessageId']
-        	message = obj[u'Message']
-        	print message
-
-        	
-
-        	conn = boto.sqs.connect_to_region("us-east-1")
-        	q = conn.get_queue('kitkat_SQS')
-        	print "made connection"
-
-    		# global resultsFromSQS
-    		tempSQS = []
-        	for i in range(3):
-        		rs = q.get_messages(message_attributes=['text','sentimentStat','geoLat','geoLong'])
-        		text = rs[0].message_attributes['text']['string_value']
-        		geoLat = rs[0].message_attributes['geoLat']['string_value']
-        		geoLong = rs[0].message_attributes['geoLong']['string_value']
-        		sentimentStat = rs[0].message_attributes['sentimentStat']['string_value']
-
-        		tempSQS.append({
-        			'text': text,
-        			'geoLat': geoLat,
-        			'geoLong': geoLong,
-        			'sentimentStat': sentimentStat
-        			})
-
-
-    			print "tempSQS = ", tempSQS
-
-    		# POSTing data to /kitkat
-    		url = 'http://webpietwittworker-dev.elasticbeanstalk.com/kitkat'
-
-    		data = {'data1': tempSQS[0], 'data2': tempSQS[1], 'data3': tempSQS[2]}
-
-    		print "data = ", data
-
-    		r = requests.post(url, data=json.dumps(data))
-
-    		return r.content
-
-    		# return '', 200
-
-
-	else:
-		return '', 404
+            print "tempSQS = ", tempSQS
+            url = 'http://160.39.131.181:9090/kitkat'
+            data = {'data1': tempSQS[0], 'data2': tempSQS[1], 'data3': tempSQS[2]}
+            print "data = ", data
+            # return '', 200
+            r = requests.post(url, data=json.dumps(data))
+            print "r.content", r.content
+            return r.content
+        print "before OK"
+        return 'OK'
 
 
 
